5182125fa988701ad3fdef26b045a17944f9154e
Pablo <pablocavalcantee@gmail.com>
2025-03-04 09:09:13 -0300

    att fotos

--- src/pages/barracas/Barracas.jsx
+++ src/pages/barracas/Barracas.jsx
@@ -1,10 +1,10 @@
 import styles from './Barracas.module.css';
 import FaqComponent from '../../components/faq/FaqComponent';
-import hotDog from '../../imgs/minihd.jpg';
-import pizza from '../../imgs/pizza.jpg';
-import pastel from '../../imgs/pastel.jpg';
+import hot from '../../imgs/hot.jpeg';
+import pizza from '../../imgs/pizzas.jpg';
+import pastel from '../../imgs/pastel.jpeg';
 import nuggets from '../../imgs/nuggets.jpg';
-import batata from '../../imgs/bt.jpg';
+import fritas from '../../imgs/fritas.jpeg';
 import hamb from '../../imgs/bf02.jpg';
 import carneLouca from '../../imgs/bf04.jpg';
 import crepe from '../../imgs/bf03.jpg';
@@ -12,11 +12,11 @@ import cascata from '../../imgs/cascata.jpg';
 
 
 const barracas = [
-  { id: 1, title: "Mini Hot - Dog", description: "Barraquinha de Mini Hot-Dog.", image: hotDog },
+  { id: 1, title: "Mini Hot - Dog", description: "Barraquinha de Mini Hot-Dog.", image: hot },
   { id: 2, title: "Mini Pizza", description: "Barraquinha de Mini Pizza.", image: pizza },
   { id: 3, title: "Mini Pastéis", description: "Barraquinha de Mini Pastéis.", image: pastel },
   { id: 4, title: "Nuggets", description: "Barraquinha de Nuggets.", image: nuggets },
-  { id: 5, title: "Batata Frita", description: "Barraquinha de Batata Frita.", image: batata },
+  { id: 5, title: "Batata Frita", description: "Barraquinha de Batata Frita.", image: fritas },
   { id: 6, title: "Hambúrguer", description: "Barraquinha de Hambúrguer.", image: hamb },
   { id: 7, title: "Carne Louca", description: "Barraquinha de Carne Louca", image: carneLouca },
   { id: 8, title: "Crepe", description: "Barraquinha de Crepe", image: crepe },
